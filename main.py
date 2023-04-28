54476537abc5e2ea9bfd64417fdd5ce4e072200f
Dogra7 <131335630+Dogra7@users.noreply.github.com>
2023-04-28 18:40:09 +0530

    commmit

--- main.py
+++ main.py
@@ -1,193 +1,165 @@
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask,request,render_template, session
+from flask import Flask, request, render_template, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 app = Flask(__name__)
-app.secret_key='deep'
+app.secret_key = "deep"
 
-engine = create_engine('postgresql://postgres:deep1234@localhost:5432/RAILWAY RESERVATION')
+engine = create_engine("postgresql://postgres:deep1234@localhost:5432/RAILWAY RESERVATION")
 Session = sessionmaker(bind=engine)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:deep1234@localhost:5432/RAILWAY RESERVATION'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:deep1234@localhost:5432/RAILWAY RESERVATION"
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-con = psycopg2.connect(database="RAILWAY RESERVATION", user="postgres", password="deep1234", host="127.0.0.1", port="5432")
+con = psycopg2.connect(
+    database="RAILWAY RESERVATION",
+    user="postgres",
+    password="deep1234",
+    host="127.0.0.1",
+    port="5432",
+)
 cursor = con.cursor()
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
     #  return 'Hello, World!!!'
-    
+
 
 class Passenger(db.Model):
-  __tablename__='Passengers'
-  id=db.Column(db.Integer,primary_key=True)
-  name=db.Column(db.String(40))
-  number=db.Column(db.Integer)
-  fromstation=db.Column(db.String(40))
-  tostation=db.Column(db.String(40))
-  dt=db.Column(db.Date)
-  seats=db.Column(db.Integer)
-  train_name=db.Column(db.String(40))
-  
-  def __init__(self,name, number, fromstation, tostation, dt, seats, train_name):
-   self.name=name
-   self.number=number
-   self.fromstation=fromstation
-   self.tostation=tostation
-   self.dt=dt
+    __tablename__ = "Passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    number = db.Column(db.Integer)
+    fromstation = db.Column(db.String(40))
+    tostation = db.Column(db.String(40))
+    dt = db.Column(db.Date)
+    seats = db.Column(db.Integer)
+    train_name = db.Column(db.String(40))
 
-   self.seats=seats
-   self.train_name=train_name
- 
- 
+    def __init__(self, name, number, fromstation, tostation, dt, seats, train_name):
+        self.name = name
+        self.number = number
+        self.fromstation = fromstation
+        self.tostation = tostation
+        self.dt = dt
 
-@app.route("/", methods=['post'])
-   
-def test(): 
-    
+        self.seats = seats
+        self.train_name = train_name
+
+
+@app.route("/", methods=["post"])
+def test():
+
     avl = request.form.get("input_fld")
-    frm=request.form.get("origin")
+    frm = request.form.get("origin")
     # cursor.execute("SELECT * from trains WHERE destination ILIKE %s OR origin ILIKE %s  ORDER BY train_name ASC",(avl, frm,))
     # result = cursor.fetchall()
     # cursor.execute("SELECT train_name from trains WHERE destination ILIKE %s OR origin ILIKE %s  ORDER BY train_name ASC",(avl, frm,))
     # trains = cursor.fetchall()
-    
 
-    result="a"
-    
+    result = "a"
+
     if avl and frm:
-     cursor.execute("SELECT * from trains WHERE destination ILIKE %s AND origin ILIKE %s AND avail_seats > 0  ORDER BY train_name ASC",(avl, frm,))
-     result = cursor.fetchall()
-     cursor.execute("SELECT train_name from trains WHERE destination ILIKE %s AND origin ILIKE %s AND avail_seats > 0  ORDER BY train_name ASC",(avl, frm,))
-     trains = cursor.fetchall()
+        cursor.execute(
+            "SELECT * from trains WHERE destination ILIKE %s AND origin ILIKE %s AND avail_seats > 0  ORDER BY train_name ASC",(avl,frm,),)
+        result = cursor.fetchall()
+        cursor.execute("SELECT train_name from trains WHERE destination ILIKE %s AND origin ILIKE %s AND avail_seats > 0  ORDER BY train_name ASC",(avl,frm,),)
+        trains = cursor.fetchall()
     elif avl:
-      cursor.execute("SELECT * from trains WHERE destination ILIKE %s ORDER BY train_no ASC",(avl,))
-      result = cursor.fetchall()
-      cursor.execute("SELECT train_name from trains WHERE destination ILIKE %s",(avl,))
-      trains = cursor.fetchall()
+        cursor.execute("SELECT * from trains WHERE destination ILIKE %s ORDER BY train_no ASC",(avl,),)
+        result = cursor.fetchall()
+        cursor.execute("SELECT train_name from trains WHERE destination ILIKE %s", (avl,))
+        trains = cursor.fetchall()
     elif frm:
-      cursor.execute("SELECT * from trains WHERE origin ILIKE %s ORDER BY train_no ASC",(frm,))
-      result = cursor.fetchall()
-      cursor.execute("SELECT train_name from trains WHERE origin ILIKE %s",(frm,))
-      trains = cursor.fetchall()
+        cursor.execute("SELECT * from trains WHERE origin ILIKE %s ORDER BY train_no ASC", (frm,))
+        result = cursor.fetchall()
+        cursor.execute("SELECT train_name from trains WHERE origin ILIKE %s", (frm,))
+        trains = cursor.fetchall()
     else:
-      result=""
-   
+        result = ""
 
-    cursor.execute("SELECT * FROM trains ORDER BY train_no ASC")    
-    all=cursor.fetchall()
-    if request.method == 'POST':
+    cursor.execute("SELECT * FROM trains ORDER BY train_no ASC")
+    all = cursor.fetchall()
+    if request.method == "POST":
 
-        
-        if 'booking' in request.form:
-          msg=" "
-          return render_template ("searchtrains.html",msg=msg)
-          
-        if request.method == 'POST':
-         session['input_fld'] = request.form.get("input_fld")
-         session['origin'] = request.form.get("origin")
+        if "booking" in request.form:
+            msg = " "
+            return render_template("searchtrains.html", msg=msg)
+
+        if request.method == "POST":
+            session["input_fld"] = request.form.get("input_fld")
+            session["origin"] = request.form.get("origin")
         else:
-         session.setdefault= ("input_fld","")
-         session.setdefault= ("origin","")
-        
-        if 'ticket' in request.form:
-          return render_template("booking.html", trains=trains)   
-        
-        if 'home' in request.form:
-          return render_template("home.html")
-        
-        if 'trains' in request.form:
-          return render_template("trainschedule.html", data=all)
-        
- 
-        
+            session.setdefault = ("input_fld", "")
+            session.setdefault = ("origin", "")
 
-                 
-        
+        if "ticket" in request.form:
+            return render_template("booking.html", trains=trains)
+
+        if "home" in request.form:
+            return render_template("home.html")
+
+        if "trains" in request.form:
+            return render_template("trainschedule.html", data=all)
+
         # if avl is None:
-          
-        #   return render_template ("test.html",msg=msg)
-        elif 'avl_trn' in request.form:
-          
-          if len(result) == 0:
-            
-            message = "TRAINS NOT AVAILABLE"
-            return render_template("searchtrains.html", message=message)
-          else:
-            return render_template("searchtrains.html", data=result)
-        
-            
-            
 
-    
+        #   return render_template ("test.html",msg=msg)
+        elif "avl_trn" in request.form:
+
+            if len(result) == 0:
+
+                message = "TRAINS NOT AVAILABLE"
+                return render_template("searchtrains.html", message=message)
+            else:
+                return render_template("searchtrains.html", data=result)
+
         # Get the form data
-        train_name = request.form['train_name']
-        num_seats = request.form['seats']
-        
+        train_name = request.form["train_name"]
+        num_seats = request.form["seats"]
+
         # Update the available seats in the 'Trains' table
-        cursor.execute("UPDATE Trains SET avail_seats = avail_seats - %s WHERE train_name = %s", (num_seats, train_name))
-        
+        cursor.execute("UPDATE Trains SET avail_seats = avail_seats - %s WHERE train_name = %s",(num_seats, train_name),)
+
         # Commit the changes to the database
         con.commit()
-        
-        
-        
-        # input_id = request.form['input_id']
-        # cursor.execute("SELECT train_name FROM trains WHERE destination = %s", (input_id,))
-        # available_trains = [row[0] for row in cursor.fetchall()]
-        # if 'submit' in request.form:
-        #  return render_template("booking.html", available_trains=available_trains)
-        # else:
-        #     return render_template("booking.html")
-        
-        
-        
-    
-    
-    
-        
-    
-        
-    name=request.form['name'] 
-    number=request.form['number']
-    fromstation=request.form['fromstation']
-    tostation=request.form['tostation']
-    dt=request.form['dt']
-    seats=request.form['seats']
-    train_name=request.form['train_name']
-        
-          
-    passenger=Passenger(name, number, fromstation, tostation, dt, seats, train_name)
+
+    name = request.form["name"]
+    number = request.form["number"]
+    fromstation = request.form["fromstation"]
+    tostation = request.form["tostation"]
+    dt = request.form["dt"]
+    seats = request.form["seats"]
+    train_name = request.form["train_name"]
+
+    passenger = Passenger(name, number, fromstation, tostation, dt, seats, train_name)
     db.session.add(passenger)
     db.session.commit()
-        
-    passengerResult=db.session.query(Passenger).filter(Passenger.id==1)
+
+    passengerResult = db.session.query(Passenger).filter(Passenger.id == 1)
     for result in passengerResult:
-      print(result.name,result.number, result.fromstation, result.tostation, result.dt, result.seats, result.train_name)
-      
-    if 'BOOK' in request.form:
-      return render_template('done.html', data=(name, fromstation, tostation, dt, seats, train_name))
-            
-    
+        print(
+            result.name,
+            result.number,
+            result.fromstation,
+            result.tostation,
+            result.dt,
+            result.seats,
+            result.train_name,
+        )
 
-      
-    
- 
-    
-    
+    if "BOOK" in request.form:
+        return render_template(
+            "done.html", data=(name, fromstation, tostation, dt, seats, train_name)
+        )
 
-   
-   
-        
-   
+
 if __name__ == "__main__":
-     app.run(debug=True)
-     
-
+    app.run(debug=True)
